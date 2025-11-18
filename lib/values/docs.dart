9612dbe40236f0efdeaaf87c06c7ad48abe5dba2
AmrAshraf-dev <amrashraf777aa@gmail.com>
2025-11-18 14:40:46 +0200

    new changes

--- lib/values/docs.dart
+++ lib/values/docs.dart
@@ -2,10 +2,12 @@ part of values;
 
 class DocumentPath {
   //Docs route
-  static const String docDir = "assets/assets/images";
+  static const String docDir = "assets/images";
   // static const String docDir = "assets/documents";
 
   //Docs
-  static const String CV = "$docDir/david-cobbina-cv.pdf";
-  static const String CV_2 = "$docDir/DAVID_COBBINA_CV.pdf";
+  static const String CV = "$docDir/Amr Ashraf - Senior Flutter Developer.pdf";
+  //static const String CV_2 = "$docDir/DAVID_COBBINA_CV.pdf";
+  static const String CV_2 =
+      "$docDir/Amr Ashraf - Senior Flutter Developer.pdf";
 }
